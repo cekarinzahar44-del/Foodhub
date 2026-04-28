8a2acbd7fab2de11a2f22c5d0f7a182ca4743535
cekarinzahar44-del <cekarinzahar44@gmail.com>
2026-04-28 14:06:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -52,13 +52,36 @@ bot.on('web_app_data', async (ctx) => {
 
 // API меню
 app.get('/api/menu', (req, res) => {
-  res.json([
-    { id: 1, name: 'Чизбургер', price: 350, img: '🍔' },
-    { id: 2, name: 'Пицца', price: 600, img: '🍕' },
-    { id: 3, name: 'Кола', price: 150, img: '🥤' },
-    { id: 4, name: 'Фри', price: 200, img: '🍟' }
-  ]);
-});
+res.json([
+  { 
+    id: 1, 
+    name: 'Чизбургер Классик', 
+    desc: 'Сочная говядина, сыр чеддер, свежие овощи',
+    price: 350, 
+    image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=400&h=300&fit=crop' 
+  },
+  { 
+    id: 2, 
+    name: 'Пицца Маргарита', 
+    desc: 'Томатный соус, моцарелла, свежий базилик',
+    price: 600, 
+    image: 'https://images.unsplash.com/photo-1574071318508-1cdbab80d002?w=400&h=300&fit=crop' 
+  },
+  { 
+    id: 3, 
+    name: 'Кола 0.5л', 
+    desc: 'Классический освежающий вкус',
+    price: 150, 
+    image: 'https://images.unsplash.com/photo-1622483767028-3f66f32aef97?w=400&h=300&fit=crop' 
+  },
+  { 
+    id: 4, 
+    name: 'Картофель фри', 
+    desc: 'Золотистый, хрустящий, с морской солью',
+    price: 200, 
+    image: 'https://images.unsplash.com/photo-1585109649139-366815a0d713?w=400&h=300&fit=crop' 
+  }
+]);
 
 // Запуск с защитой от крашей
 bot.catch((err) => console.error('🤖 Telegraf error:', err));
